ba0af0575a536ae9086eb85c5c5de0c12abf14f3
raj <rajexcited@gmail.com>
2014-03-22 23:57:37 -0400

    Revert "3rd pass"
    This reverts commit 77227963754b18c206bfea5cbd0bb927167b9227.

--- js/common.js
+++ js/common.js
@@ -1,6 +1,29 @@
 var timerElementObj, notifyTimes = [5];
 
 // angular constructor
+(function () {
+	angular.module('bspApp',['timer'])
+			.controller('timeController',function($scope,$timeout,$log) {
+				$scope.$on('timer-tick',function (event,args) {
+					$timeout(function() {
+						// notify on specified time and do soemthing
+						var ticks = args.millis/1000;
+						notifyTimes.forEach(function (item, index, list) {
+							if(item === ticks) {								
+								warningRedToggle(list,index,ticks);	
+							}
+						});
+					});
+				})
+				$scope.examTimeSeconds = 75*60;	// 60 minutes
+			})
+			.filter('twoDigit',function() {
+				return function(num) {
+					num = num ? num : '';
+					return ("00"+num).slice(-2);
+				};
+			});
+})();
 
 function countdownAction (actionTag) {
 	if(!timerElementObj) {
@@ -20,7 +43,7 @@ function warningRedToggle(list,index,oldTime) {
 
 /*	home page	*/
 
-function exitFrmApp() {
+function close () {
 	alert(navigator.app);
 	alert(navigator.device);
 }
