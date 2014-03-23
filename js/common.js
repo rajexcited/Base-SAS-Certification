77227963754b18c206bfea5cbd0bb927167b9227
raj <rajexcited@gmail.com>
2014-03-22 23:53:45 -0400

    3rd pass
    adding function - exit app alert testing

--- js/common.js
+++ js/common.js
@@ -1,29 +1,6 @@
 var timerElementObj, notifyTimes = [5];
 
 // angular constructor
-(function () {
-	angular.module('bspApp',['timer'])
-			.controller('timeController',function($scope,$timeout,$log) {
-				$scope.$on('timer-tick',function (event,args) {
-					$timeout(function() {
-						// notify on specified time and do soemthing
-						var ticks = args.millis/1000;
-						notifyTimes.forEach(function (item, index, list) {
-							if(item === ticks) {								
-								warningRedToggle(list,index,ticks);	
-							}
-						});
-					});
-				})
-				$scope.examTimeSeconds = 75*60;	// 60 minutes
-			})
-			.filter('twoDigit',function() {
-				return function(num) {
-					num = num ? num : '';
-					return ("00"+num).slice(-2);
-				};
-			});
-})();
 
 function countdownAction (actionTag) {
 	if(!timerElementObj) {
@@ -43,7 +20,7 @@ function warningRedToggle(list,index,oldTime) {
 
 /*	home page	*/
 
-function close () {
+function exitFrmApp() {
 	alert(navigator.app);
 	alert(navigator.device);
 }
